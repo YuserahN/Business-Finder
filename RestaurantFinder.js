f9d964d0fd051ffcaacf42e31a85c76d86699d52
roxasj165 <35750119+roxasj165@users.noreply.github.com>
2019-04-22 21:34:34 -0500

    My location marker and store address
    Added current location marker and store address. Address needs some tweaking.

--- RestaurantFinder.js
+++ RestaurantFinder.js
@@ -4,7 +4,7 @@
 
 var map; 
 var infoWindow;
-
+//var geocoder;
 var request;
 var service;
 var markers = [];
@@ -19,14 +19,29 @@ function initialize() {
         var center = {lat: latitude, lng: longitude}
         map.setCenter(center)
         clearResults(markers)
+
+        //Set Current Location Marker
+		var myLatLng = {lat: latitude, lng: longitude};	
+		var myMarker = new google.maps.Marker({
+          position: myLatLng,
+          map: map,
+        });
+
+        //Show CurrLocMarker Details When Pressed
+        google.maps.event.addListener(myMarker, 'click', function () {
+            infoWindow.setContent('<div><strong>' + "Current Location" + '</strong><br>');
+            infoWindow.open(map, this);
+        });
+
         var request = {
             location: center, 
-            radius: 8047, 
+            radius: 8047,
             types: ['restaurant']
         };
+
         service.nearbySearch(request, callback);
     });
-    
+	
     var center = {lat: 41.8333925, lng: -88.0121486};
     map = new google.maps.Map(document.getElementById('map'), {
         center: center,
@@ -35,50 +50,82 @@ function initialize() {
 
     request = {
         location: center,
-        radius: 8047, 
+        radius: 8047,
         types: ['restaurant']
     };
+    
+    //geocoder = new google.maps.Geocoder;
+
     infoWindow = new google.maps.InfoWindow();
 
     service = new google.maps.places.PlacesService(map);
 
     service.nearbySearch(request, callback);
 
+    //Optional Code (Clear and Center)
     google.maps.event.addListener(map, 'rightclick', function(event) {
         map.setCenter(event.latLng)
         clearResults(markers)
 
         var request = {
             location: event.latLng, 
-            radius: 8047, 
+            radius: 8047,
             types: ['restaurant']
         };
         service.nearbySearch(request, callback);
-    })
+    });
 }
 
 function callback(results, status) {
     if(status == google.maps.places.PlacesServiceStatus.OK) {
-        for(var i = 0; i < results.length; i++) {
+        for (var i = 0; i < results.length; i++) {
                 markers.push(createMarker(results[i]));
         }
     }
 }
 
 function createMarker(place) {
-    var placeLoc = place.geometry.location;
+    var address = "Press again"
+
+    //var latlng = { lat: place.geometry.location.lat(), lng: place.geometry.location.lng() };
+
+    //Request Place Details, placeId Required
+    var request = {
+        placeId: place.place_id,
+        fields: ['name', 'formatted_address', 'place_id'],
+    };
+
     var marker = new google.maps.Marker({
-        map: map, 
+        map: map,
         position: place.geometry.location
     });
 
-    google.maps.event.addListener(marker, 'click', function() {
-        infoWindow.setContent(place.name);
+    //geocoder.geocode({ 'location': latlng }, function (results, status) {
+    //    if (status === 'OK') {
+    //        if (results[0]) {
+    //            address = results[0].formatted_address
+    //        }
+    //    } else {
+    //        address = "Geocoder failed: " + status
+    //    }
+    //});
+
+    //Show Store Name and Address When Pressed (Somehow marker needs to be pressed 2x to show address)
+    google.maps.event.addListener(marker, 'click', function () {   
+        service.getDetails(request, function (place, status) {
+            address = place.formatted_address;
+        });    
+
+        infoWindow.setContent('<div><strong>' + place.name + '</strong><br>' +
+            address + '</div>');
         infoWindow.open(map, this);
     });
+
+
+
     return marker;
 }
-            
+
 function clearResults(markers) {
     for (var m in markers) {
         markers[m].setMap(null)
