7c93c671a9b06b803f3047c813e273aca5dd93fe
roxasj165 <35750119+roxasj165@users.noreply.github.com>
2019-04-30 16:03:41 -0500

    Radius Input
    Added working radius input field

--- RestaurantFinder.js
+++ RestaurantFinder.js
@@ -4,65 +4,38 @@
 
 var map; 
 var infoWindow;
-//var geocoder;
 var request;
 var service;
 var markers = [];
 var address;
+var radiusInput;
+var autocomplete;
+var latitude;
+var longitude;
+var center;
+var miles;
 
 function initialize() {
     var input = document.getElementById('searchBox');
-    var autocomplete = new google.maps.places.Autocomplete(input);
-    google.maps.event.addListener(autocomplete, 'place_changed', function () {
-        var place = autocomplete.getPlace();
-        var latitude = place.geometry.location.lat();
-        var longitude = place.geometry.location.lng();
-        var center = {lat: latitude, lng: longitude}
-        map.setCenter(center)
-        clearResults(markers)
-
-        //Set Current Location Marker
-		var myLatLng = {lat: latitude, lng: longitude};	
-		var myMarker = new google.maps.Marker({
-          position: myLatLng,
-          map: map,
-        });
-
-        //Show CurrLocMarker Details When Pressed
-        google.maps.event.addListener(myMarker, 'click', function () {
-            infoWindow.setContent('<div><strong>' + "Current Location" + '</strong><br>');
-            infoWindow.open(map, this);
-        });
-
-        var request = {
-            location: center, 
-            radius: 8047,
-            types: ['restaurant']
-        };
-
-        service.nearbySearch(request, callback);
-    });
+    autocomplete = new google.maps.places.Autocomplete(input);	
 	
-    var center = {lat: 41.8333925, lng: -88.0121486};
+	google.maps.event.addListener(autocomplete, 'place_changed', function () {		
+		var place = autocomplete.getPlace();
+		latitude = place.geometry.location.lat();
+		longitude = place.geometry.location.lng();
+		center = {lat: latitude, lng: longitude}
+		map.setCenter(center)
+		clearResults(markers)
+		
+		showCurrentLocation();
+	});
+		
+	center = {lat: 41.8333925, lng: -88.0121486};
     map = new google.maps.Map(document.getElementById('map'), {
         center: center,
         zoom: 13
     });
-
-    request = {
-        location: center,
-        radius: 8047,
-        types: ['restaurant']
-    };
-    
-    //geocoder = new google.maps.Geocoder;
-
-    infoWindow = new google.maps.InfoWindow();
-
-    service = new google.maps.places.PlacesService(map);
-
-    service.nearbySearch(request, callback);
-
+	
     //Optional Code (Clear and Center)
     google.maps.event.addListener(map, 'rightclick', function(event) {
         map.setCenter(event.latLng)
@@ -70,11 +43,55 @@ function initialize() {
 
         var request = {
             location: event.latLng, 
-            radius: 8047,
+            radius: radiusInput,
             types: ['restaurant']
         };
         service.nearbySearch(request, callback);
     });
+}
+
+//Start search after address and radius filled
+function startSearch() {
+	radiusInput = document.getElementById('searchBox2').value;
+	
+	//Convert miles to meters
+	radiusInput = radiusInput / 0.00062137;
+
+	//center = {lat: 41.8333925, lng: -88.0121486};
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: center,
+        zoom: 13
+    });
+	
+	showCurrentLocation();
+	
+    request = {
+        location: center,
+        radius: radiusInput,
+        types: ['restaurant']
+    };
+
+	infoWindow = new google.maps.InfoWindow();
+	
+	service = new google.maps.places.PlacesService(map);
+	
+    service.nearbySearch(request, callback);
+}
+
+//Put a marker that shows current location
+function showCurrentLocation() {
+	//Set Current Location Marker
+	var myLatLng = {lat: latitude, lng: longitude};	
+	var myMarker = new google.maps.Marker({
+	  position: myLatLng,
+	  map: map,
+	});
+	
+	//Show CurrLocMarker Details When Pressed
+	google.maps.event.addListener(myMarker, 'click', function () {
+		infoWindow.setContent('<div><strong>' + "Current Location" + '</strong><br>');
+		infoWindow.open(map, this);
+	});
 }
 
 function callback(results, status) {
@@ -86,9 +103,7 @@ function callback(results, status) {
 }
 
 function createMarker(place) {
-
-    //var latlng = { lat: place.geometry.location.lat(), lng: place.geometry.location.lng() };
-
+	
     //Request Place Details, placeId Required
     var request = {
         placeId: place.place_id,
@@ -99,16 +114,6 @@ function createMarker(place) {
         map: map,
         position: place.geometry.location
     });
-
-    //geocoder.geocode({ 'location': latlng }, function (results, status) {
-    //    if (status === 'OK') {
-    //        if (results[0]) {
-    //            address = results[0].formatted_address
-    //        }
-    //    } else {
-    //        address = "Geocoder failed: " + status
-    //    }
-    //});
 
     //Show Store Name and Address When Pressed
     google.maps.event.addListener(marker, 'click', function () {
